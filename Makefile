ee49b858d54269898b3bda49f182cfa539c3e468
Mateusz Puczyński <mati6095@gmail.com>
2023-10-05 00:31:11 +0200

    checkpoint; I f***** up something and nix develop does not work now

--- Makefile
+++ Makefile
@@ -5,3 +5,19 @@ container-image:
 PHONY: run
 run:
 	docker run -it -p 8080:8080/tcp --rm amostutorial
+
+.PHONY:
+build-binary-nix:
+	nix build
+
+# does not work - no crosscompilation
+.PHONY: build-container-nix
+build-container-nix:
+	nix build .#dockerImage
+
+.PHONY: develop
+develop:
+	nix develop
+
+build-container-nix-inside:
+	docker build -t amostutorial-nix . -f Dockerfile.flake
--- flake.nix
+++ flake.nix
@@ -1,5 +1,13 @@
 {
   inputs = {
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
@@ -10,13 +18,15 @@
       };
     };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
             inherit system overlays;
+                # 👇 yolo
+            config.allowUnfree = true;
           };
           # rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           # rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable."1.71.0".default;
@@ -24,13 +34,43 @@
             # new! 👇
           nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
           # also new! 👇
-          buildInputs = with pkgs; [ openssl darwin.apple_sdk.frameworks.SystemConfiguration ];
+          # buildInputs = with pkgs; [] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ]; #TODO fix me
+          buildInputs = with pkgs; [ darwin.apple_sdk.frameworks.SystemConfiguration ];
+          # this is how we can tell crane to use our toolchain!
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          # cf. https://crane.dev/API.html#libcleancargosource
+          src = craneLib.cleanCargoSource ./.;
+                # because we'll use it for both `cargoArtifacts` and `bin`
+          commonArgs = {
+            inherit src buildInputs nativeBuildInputs;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          # remember, `set1 // set2` does a shallow merge:
+          bin = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+          });
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "amostutorial";
+            tag = "latest";
+            copyToRoot = [ bin ];
+            config = {
+              Cmd = [ "${bin}/bin/amostutorial" ];
+            };
+          };
         in
         with pkgs;
         {
+          packages =
+            {
+              # that way we can build `bin` specifically,
+              # but it's also the default.
+              inherit bin dockerImage;
+              default = bin;
+            };
           devShells.default = mkShell {
-            # 👇 and now we can just inherit them
-            inherit buildInputs nativeBuildInputs;
+            # instead of passing `buildInputs` / `nativeBuildInputs`,
+            # we refer to an existing derivation here
+            inputsFrom = [ bin ];
           };
         }
       );
